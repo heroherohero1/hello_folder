1fb8ff360b98e7360c1da9e81af933511b42c522
heroherohero1 <51186837+heroherohero1@users.noreply.github.com>
2019-05-30 11:14:16 +0530

    Add files via upload

---
+++ pythoon/untitled0.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 30 11:12:43 2019
+
+@author: sujeet boran
+"""
+
+print('heloo')
---
+++ pythoon/untitled1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 30 11:13:09 2019
+
+@author: sujeet boran
+"""
+
+print('hello to all')
+a=5
+print(a)
